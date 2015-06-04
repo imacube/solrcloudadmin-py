4bd324131e3c9f16d57bf70bc85000c50dc6dfa1
imacube <ryan@imacube.net>
2015-06-04 10:32:53 -0700

    Added filters for node name and replica count; increased verbosity

--- get_replica_count.py
+++ get_replica_count.py
@@ -2,6 +2,7 @@
 Determine how many replicas are assigned to each Solr node.
 """
 
+from __future__ import print_function
 import logging
 import json
 
@@ -11,7 +12,6 @@ def load_json_file(json_file):
     """
     Load the cluster data from a JSON file.
     """
-    import json
     with open(json_file) as data_file:
         data = json.load(data_file)
     return data
@@ -34,7 +34,11 @@ def get_replica_count(collection_data):
     for collection, value in collection_data.items():
         collection_count[collection] = dict()
         for shard, data in value['shards'].items():
-            collection_count[collection][shard] = len(data['replicas'])
+            replica_dict = dict()
+            for replica, core in data['replicas'].items():
+                replica_dict[replica] = core['node_name']
+            collection_count[collection][shard] = replica_dict
+
     return collection_count
 
 def parse_arguments():
@@ -43,7 +47,7 @@ def parse_arguments():
     """
     import argparse
     parser = argparse.ArgumentParser(
-        description='Delete the specified collection replica.'
+        description='Get the summary and list of collection replicas across the cluster'
         )
     parser.add_argument(
         '--url', nargs=1, dest='url', required=False,
@@ -54,6 +58,16 @@ def parse_arguments():
         '--json_file', nargs=1, dest='json_file', required=False,
         type=str,
         help="""The JSON file to read the data from."""
+        )
+    parser.add_argument(
+        '--node_name', nargs=1, dest='node_name', required=False,
+        type=str,
+        help="""Only results that have a replica on this node name will be returned."""
+        )
+    parser.add_argument(
+        '--replica_count', nargs=1, dest='replica_count', required=False,
+        type=int,
+        help="""Only results that have at least replica count will be returned."""
         )
     parser.add_argument(
         '--debug', action='store_true', required=False,
@@ -94,19 +108,35 @@ def main():
         sys.path.append('solrcloudadmin')
         from solrcloudadmin import SolrCloudAdmin
 
-    solr_cloud = None
     if args.debug:
         configure_logging('get_replica_count', log_level=logging.DEBUG)
     else:
         configure_logging('get_replica_count', log_level=logging.INFO)
 
-    if 'url' in vars(args):
-        if args.url:
-            solr_cloud = SolrCloudAdmin(url=args.url[0], log_level=logging.INFO)
+    if 'url' in vars(args) and args.url:
+        solr_cloud = SolrCloudAdmin(url=args.url[0], log_level=logging.INFO)
+    else:
+        solr_cloud = None
 
-    if 'json_file' in vars(args):
-        if args.json_file:
-            json_file = args.json_file[0]
+    if 'json_file' in vars(args) and args.json_file:
+        json_file = args.json_file[0]
+    else:
+        json_file = None
+
+    if 'node_name' in vars(args) and args.node_name:
+        filter_node_name = args.node_name[0]
+    else:
+        filter_node_name = None
+
+    if 'replica_count' in vars(args) and args.replica_count:
+        replica_count = args.replica_count[0]
+    else:
+        replica_count = None
+
+    if not ('url' in vars(args) and args.url) and not\
+        ('json_file' in vars(args) and args.json_file):
+        LOGGER.critical('Need to set either --url or --json_file')
+        return
 
     LOGGER.info('Loading collection states')
     collection_data = get_collection_data(solr_cloud=solr_cloud, json_file=json_file)
@@ -114,9 +144,26 @@ def main():
     LOGGER.info('Starting collection replica count')
     count = get_replica_count(collection_data)
     LOGGER.info('Data format:\nreplica count, collection, shard')
+
     for collection, shards in count.items():
-        for shard, replica_count in shards.items():
-            print '%s,%s,%s' % (replica_count, collection, shard)
+        filter_print = False
+        output_string = ''
+        for shard, replicas in shards.items():
+            if replica_count and len(replicas) < replica_count:
+                continue
+            output_string += 'Summary: replicas=%d, collection=%s, shard=%s\n' % (
+                len(replicas),
+                collection,
+                shard
+                )
+            for core, node_name in replicas.items():
+                output_string += 'core=%s,node_name=%s\n' % (core, node_name)
+                if filter_node_name and node_name == filter_node_name:
+                    filter_print = True
+        if filter_node_name and filter_print:
+            print(output_string, end='')
+        elif not filter_node_name:
+            print(output_string, end='')
 
 if __name__ == '__main__':
     main()
